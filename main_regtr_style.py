7235e3cb0588a5ac041fca54230444bb6ebe3708
aniket_thor <aniket.tcdav@gmail.com>
2025-01-10 18:56:15 -0500

    Included hydra and config saving option

---
+++ main_regtr_style.py
@@ -0,0 +1,107 @@
+import torch
+from torch.utils.data import DataLoader
+from easydict import EasyDict
+import argparse
+import os
+from datasets import build_train_dataset
+from NeuStereo.neustereo import NeuStereo
+from utils.misc import load_config
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+
+    # Tensorboard summary name
+    parser.add_argument('--exp_name', default="test_single_mask", type=str)
+    parser.add_argument('--logdir', default="logs/", type=str)
+    parser.add_argument('--TIMEIT', default=False, type=bool)
+    parser.add_argument('--dev', action='store_true', default=False)
+    parser.add_argument('--skip', default=1, type=int)
+    parser.add_argument('--enable_amp', action='store_true', default=False)
+    parser.add_argument('--val_only', default=False, action='store_true')
+
+    # Model
+
+    # Dataset
+    parser.add_argument('--checkpoint_dir', default="checkpoints/", type=str)
+    parser.add_argument('--stage', default=['kitti'], type=str, nargs='+')
+    parser.add_argument('--val_dataset', default=[''], type=str, nargs='+')
+
+    # Training
+    parser.add_argument('--lr', default=0.0001, type=float)
+    parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--weight_decay', default=0.005, type=float)
+    parser.add_argument('--val_freq', default=1, type=int)
+    parser.add_argument('--epochs', default=500, type=int)
+
+    # Resume pretrained model or resume training
+    parser.add_argument('--resume', default=None, type=str)
+
+    # Distributed training
+    parser.add_argument('--distributed', action='store_true', default=False)
+
+    # Output
+    parser.add_argument('--save_output', default=False, type=bool)
+
+    return parser
+
+def setup_dataloaders(cfg, args):
+    
+
+def main(cfg, args, logger):
+    # Setup the dataloaders
+    
+
+    # Setup the model
+
+    # Setup the trainer
+
+    # Train the model
+
+
+
+if __name__ == '__main__':
+    parser = get_args_parser()
+    args = parser.parse_args()
+
+    args.local_rank = int(os.environ.get('LOCAL_RANK', 0))
+
+    if args.config is None:
+        if args.resume is None or not os.path.exists(args.resume):
+            raise ValueError("--config needs to be supplied if not resuming from a checkpoint")
+        else:
+            resume_folder = args.resume if os.path.isdir(args.resume) else os.path.dirname(args.resume)
+            args.config = os.path.normpath(os.path.join(resume_folder, "../config.yaml"))
+
+            if os.path.exists(args.config):
+                print(f"Using config file from checkpoint directory: {args.config}")
+            else:
+                raise ValueError(f"Config file not found in checkpoint directory: {args.config}")
+    
+    cfg = EasyDict(load_config(args.config))
+
+    # Store different datasets to its own subdirectory
+    # In case of training on multiple datasets, join their names with '_' and make a new directory
+    # cfg.dataset is a list of dataset names
+    logdir_name = ""
+    if len(cfg.dataset) == 1:
+        logdir_name = cfg.dataset[0]
+    else:
+        for dataset_name in cfg.dataset:
+            logdir_name += dataset_name + "_"
+
+    args.logdir = os.path.join(args.logdir, logdir_name)
+
+    if args.name is None and len(cfg.get("expt_name", "")) > 0:
+        args.name = cfg.expt_name
+    
+    logger, args.log_path = prepare_logger(args)
+
+    # Save the config file to the log directory
+    config_out_fname = os.path.join(args.log_path, "config.yaml")
+    with open(args.config, "r") as in_fid, open(config_out_fname, "w") as out_fid:
+        out_fid.write(f"Original config file name: {args.config}\n")
+        out_fid.write(in_fid.read())
+
+    main(cfg, args, logger)
+    
