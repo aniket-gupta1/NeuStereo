d16a255e8188d6a490898fd0a456844a8dd76d0a
aniket_disc <aniket@discovery>
2025-02-07 19:18:23 -0500

    Added the LayeredConv upsample module which seems to be lighter than the RAFT upsampler

--- NeuStereo/upsample.py
+++ NeuStereo/upsample.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 # from spatial_correlation_sampler import SpatialCorrelationSampler
+import pdb
 
 class UpSample(torch.nn.Module):
     def __init__(self, feature_dim, upsample_factor):
@@ -14,6 +15,7 @@ class UpSample(torch.nn.Module):
         self.relu = torch.nn.ReLU(inplace=True)
 
     def forward(self, feature, flow):
+        pdb.set_trace()
 
         concat = torch.cat((flow, feature), dim=1)
 
@@ -34,3 +36,34 @@ class UpSample(torch.nn.Module):
                                   self.upsample_factor * w)  # [B, 1, K*H, K*W]
 
         return up_flow
+
+class LayeredResizeConv(torch.nn.Module):
+    def __init__(self, dim, kernel_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conv1 = torch.nn.Conv2d(dim + 3, dim, kernel_size, padding="same")
+        self.conv2 = torch.nn.Conv2d(dim + 3, dim, kernel_size, padding="same")
+        self.conv3 = torch.nn.Conv2d(dim + 3, dim, kernel_size, padding="same")
+        # self.conv4 = torch.nn.Conv2d(dim + 3, dim, kernel_size, padding="same")
+ 
+    def apply_conv(self, source, guidance, conv, activation):
+        big_source = F.interpolate(source, scale_factor=2, mode="bilinear")
+        _, _, h, w = big_source.shape
+        small_guidance = F.interpolate(guidance, (h, w), mode="bilinear")
+        output = activation(conv(torch.cat([big_source, small_guidance], dim=1)))
+        return big_source + output
+ 
+    def forward(self, source, guidance):
+        """
+        source: initial optical flow or stereo disparity [B, 1 or 2, H//8, W//8]
+        guidance: original image guidance [B, 3, H, W]
+            Note: We can also use features from the encoder as guidance.
+
+        At each step here, we upsample the source by a factor of 2 and apply a convolutional layer.
+
+        output: upsampled optical flow or stereo disparity [B, 1 or 2, H, W]
+        """
+        source_2 = self.apply_conv(source, guidance, self.conv1, F.relu)
+        source_4 = self.apply_conv(source_2, guidance, self.conv2, F.relu)
+        source_8 = self.apply_conv(source_4, guidance, self.conv3, lambda x: x)
+        # source_16 = self.apply_conv(source_8, guidance, self.conv4, lambda x: x)
+        return source_8
