9d652ffa7756fc1e2f9a93bd139d4719cc52ac57
aniket_disc <aniket@discovery>
2025-02-07 19:17:48 -0500

    Only applying attention across the x-axis

--- NeuStereo/transformer.py
+++ NeuStereo/transformer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import pdb
 
 class TransformerLayer(torch.nn.Module):
     def __init__(self,
@@ -69,24 +70,37 @@ class FeatureAttention(torch.nn.Module):
             self.norm = torch.nn.BatchNorm2d(feature_dim)
 
     def forward(self, concat_features0):
+        # b, c, h, w = concat_features0.shape
 
-        b, c, h, w = concat_features0.shape
+        # concat_features0 = concat_features0.flatten(-2).permute(0, 2, 1)  # [B, H*W, C]
+        # concat_features1 = torch.cat(concat_features0.chunk(chunks=2, dim=0)[::-1], dim=0)
+        # for layer in self.layers:
+        #     concat_features0 = layer(concat_features0, concat_features1)
+        #     concat_features1 = torch.cat(concat_features0.chunk(chunks=2, dim=0)[::-1], dim=0)
 
-        concat_features0 = concat_features0.flatten(-2).permute(0, 2, 1)  # [B, H*W, C]
+        # # reshape back
+        # concat_features0 = concat_features0.view(b, h, w, c).permute(0, 3, 1, 2).contiguous()  # [B, C, H, W]
+        
+        # if self.post_norm:
+        #     concat_features0 = self.norm(concat_features0)
+
+        B,C,H,W = concat_features0.shape
+        concat_features0 = concat_features0.permute(0, 2, 1, 3).reshape(B * H, W, C)  # [B*H, W, C]
         concat_features1 = torch.cat(concat_features0.chunk(chunks=2, dim=0)[::-1], dim=0)
+        # pdb.set_trace()
+        # assert concat_features0.shape == (B*H, W, C) and concat_features1.shape == (B*H, W, C)
 
         for layer in self.layers:
             concat_features0 = layer(concat_features0, concat_features1)
             concat_features1 = torch.cat(concat_features0.chunk(chunks=2, dim=0)[::-1], dim=0)
 
         # reshape back
-        concat_features0 = concat_features0.view(b, h, w, c).permute(0, 3, 1, 2).contiguous()  # [B, C, H, W]
+        concat_features0 = concat_features0.view(B,H,W,C).permute(0, 3, 1, 2).contiguous()  # [B, C, H, W]
         
         if self.post_norm:
             concat_features0 = self.norm(concat_features0)
 
         return concat_features0
-
 
 class FlowAttention(torch.nn.Module):
     """
